0abf6c7e73e902566bcacd8dd0d0aec8cfb433d1
StefanTasevski1 <63559595+StefanTasevski1@users.noreply.github.com>
2020-04-12 21:56:00 +0200

    tax type variable added

--- Item.java
+++ Item.java
@@ -4,9 +4,12 @@ class Item {
 	double price;
 
 	//TODO add variable.
-
+	 float a = 0.18;
+	 float b = 0.05;
+	 int c = 0;
+	
 	//TODO constructor
-
+	
 	//TODO setters and getters
 
 	double taxReturn () {
